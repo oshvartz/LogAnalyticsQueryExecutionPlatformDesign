4dffe1e47d6c4bbe2e30e83c24ead86534918a16
Offir Shvartz <ofshvart@microsoft.com>
2021-07-25 15:30:45 +0300

    init

---
+++ Src/LogAnalyticsQueryExecutionPlatform/Contracts/IQueryResultProcessor.cs
@@ -0,0 +1,13 @@
+﻿using LogAnalyticsQueryExecutionPlatform.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogAnalyticsQueryExecutionPlatform.Contracts
+{
+    public interface IQueryResultProcessor
+    {
+        Task<ProcessingResult> ProcessQueryResultsAsync(QueryResults queryResults);
+    }
+}
